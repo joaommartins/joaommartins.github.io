566b55e8de907dee1f9ad0edd49c52bc42f2d614
João M. Martins <martins.joaomiguel@gmail.com>
2020-05-11 22:25:06 +0200

    Restructured, parsing single page parts

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
 from staticjinja import Site
+import json
+from pathlib import Path
+
+from data_reader import parse_jsons
 
 
 if __name__ == "__main__":
-    site = Site.make_site(staticpaths='static')
+    site = Site.make_site(env_globals=parse_jsons('json_data'))
     # enable automatic reloading
     site.render(use_reloader=True)
---
+++ data_reader.py
@@ -0,0 +1,25 @@
+import json
+from pathlib import Path
+
+
+def parse_jsons(json_folder):
+    '''
+    
+    '''
+    # Pre-define fields so we don't have to check in jinja
+    # Also gives the allowed filenames for json
+    page_data = {
+        'header': {'title': 'No header.json file found'},
+        'about': {'title': 'No about.json file found'},
+        'projects': {'title': 'No projects.json file found'},
+    }
+    json_files = Path(json_folder).glob('*.json')
+    for filename in json_files:
+        if filename.stem in page_data.keys():
+            with open(filename) as json_file:
+                file_contents = json.load(json_file)
+            page_data.update({filename.stem: file_contents})
+        else:
+            # Warn here?
+            continue
+    return page_data
